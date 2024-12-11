54bc5d168c24c220bcf99f0627ef0a5539fb3157
reshmic202 <reshmic202@gmail.com>
2024-12-11 14:15:50 +0530

    init

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div>
+            <div className='flex justify-between items-center px-12'>
+            <div className='cursor-pointer h-20 w-28'>
+                <img src="/logo.png" alt="" />
+            </div>
+            <div className='flex gap-10 text-xl cursor-pointer'>
+                <h1 className='font-bold'>Stays</h1>
+                <h1 className='text-gray-500'>Experiences</h1>
+            </div>
+            <div className='flex items-center gap-5'>
+                <h1 className='font-semibold'>Airbnb Your home</h1>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 21a9.004 9.004 0 0 0 8.716-6.747M12 21a9.004 9.004 0 0 1-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 0 1 7.843 4.582M12 3a8.997 8.997 0 0 0-7.843 4.582m15.686 0A11.953 11.953 0 0 1 12 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0 1 21 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0 1 12 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 0 1 3 12c0-1.605.42-3.113 1.157-4.418" />
+                </svg>
+                <div className='flex items-center border-2 rounded-3xl p-2 gap-2'>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                    </svg>
+
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                    </svg>
+
+                </div>
+
+
+            </div>
+        </div>
+        <div>
+            <div className='flex flex-col bg-gray-100 px-10  w-44 rounded-full justify-center  p-2'>
+                <label  htmlFor="">Where</label>
+                <input className=' bg-gray-100  ' type="text" placeholder='Search destinations' />
+            </div>
+        </div>
+        </div>
+    )
+}
+
+export default Navbar
